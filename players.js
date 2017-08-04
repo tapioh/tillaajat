1f1d854620dcf503e4108fa6a945709527a8a58a
Tapio Heiskanen <tafe@mymac.local>
2017-08-04 11:07:21 +0300

    Adjust player positions.

--- players.js
+++ players.js
@@ -13,18 +13,22 @@ const players = [
   { position: POSITION_LEFT_WING, number: 13, name: 'Juha Koivisto', facebookId: '' },
   { position: POSITION_LEFT_WING, number: 14, name: 'Martti Riuttamäki', facebookId: '10153378784363046' },
   { position: POSITION_LEFT_WING, number: 96, name: 'Joona Liimatainen', facebookId: '1592747054070994' },
+  { position: POSITION_LEFT_WING, number: 99, name: 'Jasu Kettunen', facebookId: '1206667409370790' },
   { position: POSITION_CENTER, number: 9, name: 'Dennis Blomqvist', facebookId: '10152430322167567' },
-  { position: POSITION_CENTER, number: 23, name: 'Tuomas Salonen', facebookId: '10210379139217043' },
   { position: POSITION_CENTER, number: 44, name: 'Tapio Heiskanen', facebookId: '10203051419748163' },
   { position: POSITION_CENTER, number: 51, name: 'Teemu Keski-Posso', facebookId: '10153158837649855' },
   { position: POSITION_CENTER, number: 88, name: 'Panu Korhonen', facebookId: '1029158183761227' },
+  { position: POSITION_CENTER, number: 10, name: 'Tatu Valkonen', facebookId: '10204171396346755' },
   { position: POSITION_RIGHT_WING, number: 18, name: 'Osku Mutanen', facebookId: '10153776320643491' },
   { position: POSITION_RIGHT_WING, number: 21, name: 'Iisakki Hyypiä', facebookId: '' },
+  { position: POSITION_RIGHT_WING, number: 23, name: 'Tuomas Salonen', facebookId: '10210379139217043' },
   { position: POSITION_RIGHT_WING, number: 22, name: 'Valtteri Ollila', facebookId: '720505497971235' },
   { position: POSITION_RIGHT_WING, number: 33, name: 'Jose Koivisto', facebookId: '10152881542459399' },
+  { position: POSITION_RIGHT_WING, number: 77, name: 'Petri Niemi', facebookId: '1098443840182150' },
   { position: POSITION_LEFT_DEFENDER, number: 5, name: 'Joni Hyvönen', facebookId: '10203246420694981' },
   { position: POSITION_LEFT_DEFENDER, number: 47, name: 'Ville Räsänen', facebookId: '10155522656303895' },
   { position: POSITION_LEFT_DEFENDER, number: 69, name: 'Teppo Sillanpää', facebookId: '10152638265983515' },
+  { position: POSITION_LEFT_DEFENDER, number: 7, name: 'Mikko Suominen', facebookId: '10203688770202350' },
   { position: POSITION_RIGHT_DEFENDER, number: 6, name: 'Matti Partala', facebookId: '10153067241744130' },
   { position: POSITION_RIGHT_DEFENDER, number: 8, name: 'Heikki Heiskanen', facebookId: '2052971774841799' },
   { position: POSITION_RIGHT_DEFENDER, number: 15, name: 'Tomi Johansson', facebookId: '10152459180293254' },
@@ -35,11 +39,7 @@ const players = [
   { position: POSITION_GOALKEEPER, number: 50, name: 'Ippe Petäjäsalo', facebookId: '' },
   { position: POSITION_GOALKEEPER, number: 91, name: 'Tommi Varis', facebookId: '' },
   { position: POSITION_GOALKEEPER, number: 98, name: 'Niklas Wentus', facebookId: '10152952128867138' },
-  { position: POSITION_OTHER, number: 0, name: 'Jasu Kettunen', facebookId: '1206667409370790' },
-  { position: POSITION_OTHER, number: 7, name: 'Mikko Suominen', facebookId: '10203688770202350' },
-  { position: POSITION_OTHER, number: 10, name: 'Tatu Valkonen', facebookId: '10204171396346755' },
-  { position: POSITION_OTHER, number: 17, name: 'Raimo Paavola', facebookId: '10155411274570772' },
-  { position: POSITION_OTHER, number: 77, name: 'Petri Niemi', facebookId: '1098443840182150' }
+  { position: POSITION_OTHER, number: 17, name: 'Raimo Paavola', facebookId: '10155411274570772' }
 ]
 
 export default players
